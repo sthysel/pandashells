4528779b9d4732910f1873fd63dbb91cb8f63acb
Rob deCarvalho <robdmc@gmail.com>
2015-07-30 14:32:16 -0400

    python2 and python3 tests passing at 100

--- pandashells/bin/p_example_data.py
+++ pandashells/bin/p_example_data.py
@@ -12,7 +12,8 @@ import pandashells
 def main():
     # create a dict of data-set names and corresponding files
     package_dir = os.path.dirname(os.path.realpath(pandashells.__file__))
-    sample_data_dir = os.path.realpath(os.path.join(package_dir, 'example_data'))
+    sample_data_dir = os.path.realpath(
+        os.path.join(package_dir, 'example_data'))
 
     f_dict = {}
     for f in os.listdir(sample_data_dir):
@@ -56,7 +57,7 @@ def main():
 
     # print contents of data file to output
     f_name = f_dict[args.dataset[0]]
-    with open(f_name, encoding='utf8') as in_file:
+    with open(f_name) as in_file:
         try:
             # line by line avoids weird sys.excepthook bug on pipe to head
             for line in in_file:
